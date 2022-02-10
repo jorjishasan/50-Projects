c8840beeb021ee3f2ca758b63f87daa94fcdcfa3
jorjis <devjorjis212@gmail.com>
2022-02-11 02:41:41 +0600

    structure,style and interaction

--- Scroll Animation/dist/script.js
+++ Scroll Animation/dist/script.js
@@ -1,1 +1,68 @@
 'use script';
+
+const gradients = [
+    "background-image: linear-gradient( 102.4deg,  rgba(253,189,85,1) 7.8%, rgba(249,131,255,1) 100.3% );",
+
+    "background-image: linear-gradient( 83.2deg,  rgba(150,93,233,1) 10.8%, rgba(99,88,238,1) 94.3% );",
+
+    "background-image: radial-gradient( circle farthest-corner at 10% 20%,  rgba(14,174,87,1) 0%, rgba(12,116,117,1) 90% );",
+
+    "background-image: linear-gradient( 83.2deg,  rgba(150,93,233,1) 10.8%, rgba(99,88,238,1) 94.3% );",
+
+    "background-image: radial-gradient( circle farthest-corner at 10% 20%,  rgba(255,209,67,1) 0%, rgba(255,145,83,1) 90% );",
+
+    "background-image: radial-gradient( circle 976px at 51.2% 51%,  rgba(11,27,103,1) 0%, rgba(16,66,157,1) 0%, rgba(11,27,103,1) 17.3%, rgba(11,27,103,1) 58.8%, rgba(11,27,103,1) 71.4%, rgba(16,66,157,1) 100.2%, rgba(187,187,187,1) 100.2% );",
+
+    "background-image: linear-gradient( 117deg,  rgba(123,216,96,1) 39.2%, rgba(255,255,255,1) 156.2% );",
+
+    "background-image: linear-gradient( 109.6deg,  rgba(45,116,213,1) 11.2%, rgba(121,137,212,1) 91.2% );",
+
+    "background-image: linear-gradient( 358.4deg,  rgba(249,151,119,1) -2.1%, rgba(98,58,162,1) 90% );",
+
+    "background-image: linear-gradient( 109.6deg,  rgba(209,0,116,1) 11.2%, rgba(110,44,107,1) 91.1% );",
+
+    "background-image: radial-gradient( circle 685.3px at 47.8% 55.1%,  rgba(255,99,152,1) 0%, rgba(251,213,149,1) 90.1% );",
+
+    "background-image: radial-gradient( circle farthest-corner at 10% 20%,  rgba(162,102,246,1) 0%, rgba(203,159,249,1) 90% );",
+
+    "background-image: radial-gradient( circle 674px at 18.3% 77%,  rgba(139,186,244,1) 3.4%, rgba(15,51,92,1) 56.6% );",
+
+    "background-image: linear-gradient( 105.3deg,  rgba(30,39,107,1) 21.8%, rgba(77,118,221,1) 100.2% );"
+];
+
+const boxes = document.querySelectorAll('.box');
+
+function shuffle(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * i);
+        let k = arr[i];
+        arr[i] = arr[j];
+        arr[j] = k;
+    }
+    return arr;
+}
+
+
+// console.log(gradients);
+
+/* 
+fisher yates shuffle: accuracy: 99% 
+
+link: https://www.w3schools.com/js/tryit.asp?filename=tryjs_array_sort_random2
+*/
+
+shuffle(gradients).forEach((color, index) => {
+    boxes[index].style = color;
+});
+
+function scrollCheckBox() {
+    const triggerPoint = window.innerHeight * 0.80;
+    boxes.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top;
+        if (triggerPoint > boxTop) box.classList.add('active');
+        else box.classList.remove('active');
+    });
+}
+
+window.onscroll = scrollCheckBox;
+scrollCheckBox();//for filling viewport height
